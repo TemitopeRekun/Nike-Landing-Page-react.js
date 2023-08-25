fa7c74639ffc09028de3b7cdb650866147af3d4d
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-25 21:41:23 +0100

    initilised the repository

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+  return (
+    <>
+      <div className="w-full p-10 bg-black mt-5">
+        <h1 className="text-white">hello world</h1>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
