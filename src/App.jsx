39b3e7f1764e36daef151f914ec4d79344533d08
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-26 13:30:15 +0100

    Arranging the file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 
 
-function App() {
-  
-  return (
-    <>
-      <div className="w-full p-10 bg-black mt-5">
-        <h1 className="text-white">hello world</h1>
-      </div>
-        
-    </>
-  )
-}
 
-export default App
+
+
+const App = () => (
+	<main className="relative">
+		nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      hero
+    </section>
+    <section className="padding">
+      PopularProduct
+    </section>
+    <section className="padding">
+      superquality
+    </section>
+    <section className="padding padding-x py-10">
+      services
+    </section>
+    <section className="padding">
+      specialoffer
+    </section>
+    <section className="padding bg-pale-blue paddding">
+			customer reviews
+		</section>
+		<section className="padding padding-x sm:py-32 py-16 w-full">
+			suscribe
+		</section>
+		<section className="padding bg-black padding-x padding-t pb-8">
+			footer
+		</section>
+	</main>
+);
+
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Suscribe from "./Suscribe";
+import Footer from "./Footer";
+
+export default {
+	Hero,
+	PopularProduct,
+	SuperQuality,
+	Services,
+	SpecialOffer,
+	CustomerReviews,
+	Suscribe,
+	Footer,
+};
