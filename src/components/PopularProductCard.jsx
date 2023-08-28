131f7f61311f94be73a8c0b8caef6b85f200838d
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-28 23:07:54 +0100

    started styling the popular products component

---
+++ src/components/PopularProductCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PopularProductCard = ({ imgURL, name, price }) => {
+    return (
+
+        <div className="">
+
+        </div>
+    );
+};
+
+export default PopularProductCard;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
-import { statistics } from "../constants";
+import { shoes, statistics } from "../constants";
 import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../components/ShoeCard";
 
 const Hero = () => {
+	const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
 	return (
 		<section
 			id="home"
@@ -43,14 +46,28 @@ const Hero = () => {
 				</div>
 			</div>
 
-			<div>
+			<div className="relative flex flex-1 justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
 				<img
-					src={bigShoe1}
+					src={bigShoeImg}
 					alt="Nike sneakers collection"
 					width={610}
 					height={500}
 					className="object-contain relative z-10"
 				/>
+
+				<div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+					{shoes.map((shoe, index) => (
+						<div key={index + 1}>
+							<ShoeCard
+								imgUrl={shoe}
+								changeBigShoeImage={(shoe) => {
+									setBigShoeImg(shoe);
+								}}
+								bigShoeImg={bigShoeImg}
+							/>
+						</div>
+					))}
+				</div>
 			</div>
 		</section>
 	);
