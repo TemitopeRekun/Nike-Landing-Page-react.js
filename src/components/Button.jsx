b626b7637c3bd33d8bd88dc5b92c1eabd39b415d
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-31 13:01:39 +0100

    styling the review component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+	label,
+	iconUrl,
+	borderColor,
+	backgroundColor,
+	textColor,
+}) => {
 	return (
-		<button type="button" className="flex justify-center items-center g-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white">
+		<button
+			type="button"
+			className={`flex justify-center  items-center border  g-2 px-7 py-4 font-montserrat text-lg leading-none
+				${
+					backgroundColor
+						? `${backgroundColor} ${textColor} ${borderColor}`
+						: "bg-coral-red  text-white border-coral-red"
+				} rounded-full`}>
 			{label}
-			{iconUrl && <img
-				src={iconUrl}
-				alt="arrowRight"
-				className="rounded-full ml-2 w-5 h-5"
-			/>}
+			{iconUrl && (
+				<img
+					src={iconUrl}
+					alt="arrowRight"
+					className="rounded-full ml-2 w-5 h-5"
+				/>
+			)}
 		</button>
 	);
 };
--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
 
 const CustomerReviews = () => {
-  return (
-    <div>CustomerReviews</div>
-  )
-}
+	return (
+		<section className="max-container">
+			<div>
+				<h3 className="font-palanquin text-center text-4xl font-bold">
+					What Our <span className="text-coral-red"> Customers</span> Say?
+				</h3>
+				<p className="info-text m-auto mt-4 max-w-lg text-center">
+					Hear genuine stories fom our satisfied customers about their
+					exceptional experiences with us.
+				</p>
 
-export default CustomerReviews
+				<div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+					{reviews.map((review) => (
+						<ReviewCard
+							key={review.customerName}
+							imgUrl={review.imgURL}
+							customerName={review.customerName}
+							rating={review.rating}
+							feedback={review.feedback}
+						/>
+					))}
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default CustomerReviews;
